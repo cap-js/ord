f8b42817ca8f4516710e5a28e388323f9d656fdb
I524884 <sidakdeep.singh@sap.com>
2024-06-05 14:33:49 +0530

    fixes

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,5 +1,5 @@
-require("./lib/plugin");
 const cds = require("@sap/cds");
+const { ord, getMetadata, defaults } = require("./lib/index");
 
 function _lazyRegisterCompileTarget() {
   const value = require("./lib/index").ord;
@@ -15,3 +15,32 @@ const registerORDCompileTarget = () => {
 };
 
 registerORDCompileTarget();
+
+cds.on("bootstrap", (app) => {
+  app.use("/.well-known/open-resource-discovery", async (req, res) => {
+    if (req.url === "/") {
+      res.status(200).send(defaults.baseTemplate);
+    } else {
+      try {
+        const { contentType, response } = await getMetadata(req.url);
+        res.status(200).contentType(contentType).send(response);
+      } catch (error) {
+        console.log(error);
+        console.log('Error while generating metadata');
+        res.status(500).send(error.message);
+      }
+    }
+  });
+
+  app.get("/open-resource-discovery/v1/documents/1", async (req, res) => {
+    try {
+      const csn = await cds.load(cds.env.folders.srv);
+      const data = ord(csn);
+      return res.status(200).send(data);
+    } catch (error) {
+      console.log(error);
+      console.log('Error while creating ORD document');
+      return res.status(500).send(error.message);
+    }
+  });
+});
