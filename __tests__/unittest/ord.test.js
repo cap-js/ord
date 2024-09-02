75d05b971e82e101937d6e216dacb07a862cad08
Zongqi Chen <zongqi.chen@sap.com>
2024-09-02 15:55:56 +0200

    add test placeholder

---
+++ __tests__/unittest/ord.test.js
@@ -0,0 +1,5 @@
+describe('ord', () => {
+    it('test placeholder', () => {
+        expect(true).toBe(true);
+    });
+});
---
+++ __tests__/unittest/plugin.test.js
@@ -0,0 +1,5 @@
+describe('plugin', () => {
+    it('test placeholder', () => {
+        expect(true).toBe(true);
+    });
+});
