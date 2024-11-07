128e49423f5f518dc93595de575f632265d3717f
Albin Ramovic <albin.ramovic@sap.com>
2024-11-07 11:57:06 +0100

    private and internal resource are not considered

--- __tests__/ordCdsrc.test.js
+++ __tests__/ordCdsrc.test.js
@@ -10,7 +10,7 @@ jest.mock("@sap/cds", () => {
 });
 
 jest.mock("../lib/date", () => ({
-    getRFC3339Date: jest.fn(() => "2024-11-04T14:33:25+01:00")
+    getRFC3339Date: jest.fn(() => "2024-11-04T14:33:25+01:00"),
 }));
 
 const cds = require("@sap/cds");
@@ -29,7 +29,8 @@ describe("Tests for default ORD document when .cdsrc.json is present", () => {
 
     describe("apiResources", () => {
         // eslint-disable-next-line no-useless-escape
-        const PACKAGE_ID_REGEX = /^([a-z0-9]+(?:[.][a-z0-9]+)*):(package):([a-zA-Z0-9._\-]+):(v0|v[1-9][0-9]*)$/;
+        const PACKAGE_ID_REGEX =
+            /^([a-z0-9]+(?:[.][a-z0-9]+)*):(package):([a-zA-Z0-9._\-]+):(v0|v[1-9][0-9]*)$/;
 
         let document;
 
@@ -39,12 +40,16 @@ describe("Tests for default ORD document when .cdsrc.json is present", () => {
 
         test("PartOfPackage values are valid ORD IDs ", () => {
             for (const apiResource of document.apiResources) {
+                if (!apiResource.partOfPackage) continue;
+
                 expect(apiResource.partOfPackage).toMatch(PACKAGE_ID_REGEX);
             }
         });
 
         test("The partOfPackage references an existing package", () => {
             for (const apiResource of document.apiResources) {
+                if (!apiResource.partOfPackage) continue;
+
                 expect(
                     document.packages.find(
                         (pck) => pck.ordId === apiResource.partOfPackage
@@ -56,7 +61,8 @@ describe("Tests for default ORD document when .cdsrc.json is present", () => {
 
     describe("eventResources", () => {
         // eslint-disable-next-line no-useless-escape
-        const GROUP_ID_REGEX = /^([a-z0-9-]+(?:[.][a-z0-9-]+)*):([a-zA-Z0-9._\-/]+):([a-z0-9-]+(?:[.][a-z0-9-]+)*):(?<service>[a-zA-Z0-9._\-/]+)$/;
+        const GROUP_ID_REGEX =
+            /^([a-z0-9-]+(?:[.][a-z0-9-]+)*):([a-zA-Z0-9._\-/]+):([a-z0-9-]+(?:[.][a-z0-9-]+)*):(?<service>[a-zA-Z0-9._\-/]+)$/;
 
         let document;
 
