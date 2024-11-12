976bbc46e688378a231115928e9a9e42cb042038
Albin Ramovic <albin.ramovic@sap.com>
2024-11-12 17:58:12 +0100

    add no-ord-cdsrc-test

--- __tests__/noOrdInCdsrc.test.js
+++ __tests__/noOrdInCdsrc.test.js
@@ -3,20 +3,24 @@ const ord = require("../lib/ord");
 const path = require("path");
 const csn = require("./__mocks__/publicResourcesCsn.json");
 
-describe("Tests for default ORD document when .cdsrc.json is present", () => {
-    beforeAll(async () => { });
+jest.mock("../lib/date", () => ({
+    getRFC3339Date: jest.fn(() => "2024-11-04T14:33:25+01:00")
+}));
 
-    beforeEach(() => {
+describe("Tests for ORD document when .cdsrc.json has no `ord` property", () => {
+    beforeAll(() => {
         cds.root = path.join(__dirname, "bookshop");
         cds.env = {};
-    });
+     });
 
-    afterEach(() => {
+    afterAll(() => {
         jest.clearAllMocks();
     });
 
-    test("Successfully create ORD Documents with defaults", () => {
+    test("Successfully create ORD Documents with no `ord` in .cdsrc.json", () => {
+
         const document = ord(csn);
         expect(document).not.toBeUndefined();
+        expect(document).toMatchSnapshot();
     });
 });
--- __tests__/protectedServices.test.js
+++ __tests__/protectedServices.test.js
@@ -4,29 +4,40 @@ const ord = require("../lib/ord");
 const path = require("path");
 const private_csn = require("./__mocks__/privateResourcesCsn.json");
 
+function checkOrdDocument(csn) {
+    const document = ord(csn);
+
+    expect(document).not.toBeUndefined();
+    expect(document.packages).not.toBeDefined();
+    expect(document.apiResources).toHaveLength(0);
+    expect(document.eventResources).toHaveLength(0);
+}
+
+
+jest.mock("../lib/date", () => ({
+    getRFC3339Date: jest.fn(() => "2024-11-04T14:33:25+01:00")
+}));
+
 describe("Tests for ORD document when there is no public service", () => {
     beforeAll(() => {
         cds.root = path.join(__dirname, "bookshop");
-        cds.env = {};
+        cds.env.ord = {
+            namespace: "sap.test.cdsrc.sample",
+            openResourceDiscovery: "1.10",
+            description: "this is my custom description",
+            policyLevel: "sap:core:v1"
+        };
     });
 
     afterAll(() => {
         jest.clearAllMocks();
-    })
+    });
 
     test("All services are private: Successfully create ORD Documents without packages, empty apiResources and eventResources lists", () => {
-        const document = ord(private_csn);
-
-        expect(document.packages).not.toBeDefined();
-        expect(document.apiResources).toHaveLength(0);
-        expect(document.eventResources).toHaveLength(0);
+        checkOrdDocument(private_csn);
     });
 
     test("All services are internal: Successfully create ORD Documents without packages, empty apiResources and eventResources lists", () => {
-        const document = ord(internal_csn);
-
-        expect(document.packages).not.toBeDefined();
-        expect(document.apiResources).toHaveLength(0);
-        expect(document.eventResources).toHaveLength(0);
+        checkOrdDocument(internal_csn);
     });
 });
