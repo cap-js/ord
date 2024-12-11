d8033dcecc91389b67d4aca284de17bb057d35f9
Roshni Naveena S <roshni.naveena.s@sap.com>
2024-12-11 13:03:44 +0530

    Update ORDService.js

--- services/ORDService.js
+++ services/ORDService.js
@@ -1,12 +1,35 @@
 const cds = require('@sap/cds')
 
-class OrdService extends cds.ApplicationService {
-  /** Registering custom event handlers */
-  init() {
-    this.on('GET', '*', console.log('hi'))
-    return super.init()
-  }
+// class OrdService extends cds.ApplicationService {
+//   /** Registering custom event handlers */
+//   init() {
+//     this.on('GET', '*', console.log('hi'))
+//     return super.init()
+//   }
 
-}
+// }
 
-module.exports = { OrdService }
+
+module.exports = cds.service.impl(async function () {
+    if (this instanceof cds.ApplicationService) {
+        //iterate over this.model.definitions and for all kind as service do this.on and listen
+        for (const [key, value] of Object.entries(this.model.definitions)) {
+            if (value.kind === "service") {
+                this.on("READ", '*', async (req) => {
+                    console.log("READ");
+                    console.log(req);
+                    return [
+                        { id: 1, name: "Order 1", status: "Pending" },
+                        { id: 2, name: "Order 2", status: "Completed" },
+                    ];
+                });
+            }
+        }
+    }
+
+
+
+});
+
+
+// module.exports = { OrdService }
