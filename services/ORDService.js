65e0c11bf952d1bdf6ee6f25c7975bd88cd15167
Roshni Naveena S <roshni.naveena.s@sap.com>
2024-12-11 13:01:37 +0530

    Update ORDService.js

--- services/ORDService.js
+++ services/ORDService.js
@@ -3,19 +3,10 @@ const cds = require('@sap/cds')
 class OrdService extends cds.ApplicationService {
   /** Registering custom event handlers */
   init() {
-    this.before('READ', '*', console.log('hi'))
+    this.on('GET', '*', console.log('hi'))
     return super.init()
   }
-
-
-
 
 }
 
 module.exports = { OrdService }
-
-
-// ------------------------------------------------------------------------------------------------------------------------------------------------------------
-// For demo purposess only...
-const _require = id => {try{ return require(id) } catch(e) { if (e.code !== 'MODULE_NOT_FOUND') throw e }}
-cds.once("served", ()=> _require('./alert-notifications')?.prototype.init.call(cds.services.OrdService))
