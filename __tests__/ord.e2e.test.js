5a724b8b7fbab82ec5d59282445720014cf0d81a
Shir <nangiale.shir@sap.com>
2025-01-10 17:09:43 +0100

    fix: Use cds.root with process.cwd fallback + Test files rollback

--- __tests__/ord.e2e.test.js
+++ __tests__/ord.e2e.test.js
@@ -22,17 +22,9 @@ describe("End-to-end test for ORD document", () => {
         });
 
         test("Exception thrown while package.json not found", () => {
-            jest.spyOn(cds.utils, "exists").mockImplementation((path) =>
-                path.endsWith("package.json") ? false : true
-            );
-        
-            const invalidModelPath = path.join(__dirname, "somePath");
-            expect(() => ord(csn, invalidModelPath)).toThrowError(
-                "package.json not found in the project root directory"
-            );
-        
-            cds.utils.exists.mockRestore();
-        });                      
+            cds.root = path.join(__dirname, "folderWithNoPackageJson");
+            expect(() => ord(csn)).toThrowError("package.json not found in the project root directory");
+        });
 
         describe("apiResources", () => {
             // eslint-disable-next-line no-useless-escape
