facf4d742d2cf1fb6cab393b5f207c3afd4b15cd
Zongqi Chen <zongqi.chen@sap.com>
2025-04-30 19:33:25 +0200

    add event, csn and move to new module build.js

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,6 +1,9 @@
 const cds = require("@sap/cds");
-const { path } = cds.utils
 const { getAuthConfig } = require("./lib/authentication");
+
+if (cds.cli.command === "build") {
+    cds.build?.register?.('ord', require("./lib/build"));
+}
 
 // load auth config before any service is started
 cds.on("bootstrap", async () => {
@@ -22,48 +25,3 @@ const registerORDCompileTarget = () => {
 
 registerORDCompileTarget();
 
-let ordPromise = import('./lib/es-module.mjs').then((esModule) => esModule.ord);
-let getMetadataPromise = import('./lib/es-module.mjs').then((esModule) => esModule.getMetadata);
-cds.build?.register?.('ord', class OrdBuildPlugin extends cds.build.Plugin {
-
-    static taskDefaults = { src: cds.env.folders.srv }
-
-    init() {
-        // different from the default build output structure
-        this.task.dest = path.join(cds.root, '_ord_gen')
-    }
-
-    async build() {
-        const model = await this.model();
-        const ord = await ordPromise;
-        const getMetadata = await getMetadataPromise;
-        const ordDocument = ord(model);
-
-        const promises = []
-        promises.push(
-            this.write(ordDocument).to('ord-document.json')
-        )
-
-        for (const apiResource of ordDocument.apiResources) {
-            // skip apiResources that are not extensible?
-            // if (apiResource.extensible?.supported === 'yes')
-
-            if (apiResource.ordId.includes('OpenResourceDiscoveryService') || !apiResource.resourceDefinitions) {
-                continue;
-            }
-
-            const subDir = path.join(this.task.dest, apiResource.ordId)
-
-            for (const resourceDefinition of apiResource.resourceDefinitions) {
-                const url = resourceDefinition.url;
-                const fileName = url.split('/').pop();
-                const { _, response } = await getMetadata(url, model); // eslint-disable-line no-unused-vars
-                promises.push(
-                    this.write(response).to(path.join(subDir, fileName))
-                )
-            }
-        }
-
-        return Promise.all(promises)
-    }
-})
