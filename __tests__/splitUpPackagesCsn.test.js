b21fcb82a0ec29f3824d616ca6f6d4006fdd6b33
Zongqi Chen <zongqi.chen@sap.com>
2025-06-25 19:52:11 +0800

    remove illegl scenario

--- __tests__/splitUpPackagesCsn.test.js
+++ __tests__/splitUpPackagesCsn.test.js
@@ -40,7 +40,7 @@ describe("ORD Generation for Business Accelerator Hub", () => {
             expect(document.openResourceDiscovery).toBe("1.10");
             expect(document.policyLevels).toEqual(["sap:core:v1"]);
             expect(document.description).toBe("Business Accelerator Hub ORD Test");
-            expect(document.apiResources).toHaveLength(6);
+            expect(document.apiResources).toHaveLength(7);
             expect(document.eventResources).toHaveLength(1);
         });
     });
